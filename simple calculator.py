f33dae8d47dd41ccb692b640fca77596622cfb7b
virencampk12 <113609747+virencampk12@users.noreply.github.com>
2022-09-15 18:45:03 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+from itertools import product
+
+
+print("simple calculator")
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+print("Sum: ",str(sum))
+print("Difference: ",str(difference))
+print("Product: ",str(product))
+print("Quotient: ",str(quotient))
